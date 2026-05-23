3b690df8f072ce3ac270a192ababe9176526fc06
blanco101 <blancovi@esat-alumni.com>
2026-05-23 10:43:55 +0200

    Use FX10 instead of FX12 + remove inner loop if
    - FX12 was performing overflow when multiplying numbers and the camera was a bit over the terrain

--- src/compile.sh
+++ src/compile.sh
@@ -6,8 +6,8 @@ fi
 
 # Para compilar en linux x86/x64
 # gcc -c -g -Wall -O0 -fno-tree-vectorize -o obj/main.o main.c
-# gcc -c -g -Wall -O2 -o obj/main.o main.c
 gcc -c -g -Wall -O2 -o obj/main.o main.c
+# gcc -c -g -Wall -O2 -fsanitize=address,undefined -o obj/main.o main.c
 # Para compilar ARM
 #gcc -c -g -Wall -mfpu=vfp -O2 -o obj/main.o main.c
 
@@ -16,7 +16,6 @@ gcc -c -g -Wall -O2 -o obj/chrono.o chrono.c
 cd obj
 
 gcc -o ../main.elf main.o chrono.o  -lm -lSDL 
-
-# gcc -c -g -Wall -O2 -fsanitize=address,undefined -o obj/main.o main.c
 # gcc -fsanitize=address,undefined -o ../main.elf main.o chrono.o  -lm -lSDL 
+
 # objdump -d -M intel obj/main.o > main.s
